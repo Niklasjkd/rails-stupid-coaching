7116c4841a5dae35c4b32c189ad0b8954fa2702b
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-08-12 14:08:13 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'question', to: 'pages#question'
+  get 'answer', to: 'pages#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
